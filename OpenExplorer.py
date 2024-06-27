34ddc30a49ed63e3c2a773972f00333a31cc0fcd
Distroff <Teryokhin@yandex.ru>
2024-06-27 15:23:02 +0300

    add file

--- OpenExplorer.py
+++ OpenExplorer.py
@@ -5,10 +5,11 @@ def explorer_on_file(url):
     subprocess.Popen(f'explorer /select,"{url}"')
 
 file_name = '2B21167600_Дон Кихот_20240203.mov'
-# file_path = r"V:\DATA\2\B\2\2B21167600_Дон Кихот_20240203.mov"
+file_path = r"\\?\V:\DATA\2\B\2\2B21167600_Дон Кихот_20240203.mov"
+# file_path = r"V:/DATA/2/B/2/2B21167600_Дон Кихот_20240203.mov"
 # file_path = r"\\big1\VIDEO\DATA\2\B\2\2B21167600_Дон Кихот_20240203.mov"
 # file_path = r'C:\adb\adb.exe'
 
 explorer_on_file(file_path)
 
-print(os.listdir(r'V:\DATA'))
+print(os.listdir(r'V:\DATA\2\B'))
